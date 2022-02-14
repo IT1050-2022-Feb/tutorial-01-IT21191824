c8dc9b4af39147de71bda4bd0bc5882f1ba361d9
IT21191824 <99653011+IT21191824@users.noreply.github.com>
2022-02-14 20:06:36 +0530

    Update Tute03.c

--- Tute03.c
+++ Tute03.c
@@ -12,8 +12,21 @@ n -> 1-
 sum = 1+2+3+...+10 = 55 */
 
 #include <stdio.h>
-int main() {
-  
+ 
+int main()
+{
+  int n, sum = 0, c;
+ 
+  printf("How many numbers you want to add?\n");
+  scanf("%d", &n);
+ 
+  for (c = 1; c <= n; c++)
+  {
+    sum = sum + c;
+  }
+ 
+  printf("Sum of the integers = %d\n", sum);
+ 
   return 0;
 }
 
